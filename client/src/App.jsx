ed2c24fb0fd7d12a603541ffbbfc069c15cc6e0e
lamba01 <jamesspaul987@gmail.com>
2025-06-07 13:13:06 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-green-700 text-3xl">flight boking</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
